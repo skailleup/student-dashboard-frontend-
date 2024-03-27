437d32bb7026364da6cdc15b493f35d833dc20ae
Adetunji Igbatayo <adetunjiigbatayo@gmail.com>
2024-03-27 20:45:56 +0100

    feat: complete slicing dashboard page

--- src/components/ui/spinner/index.tsx
+++ src/components/ui/spinner/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function LoadingSpinner() {
+interface LoadingSpinnerProps {
+  type?: "primary" | "secondary";
+}
+
+export default function LoadingSpinner({ type = "primary" }: LoadingSpinnerProps) {
   return (
     <div
-      className="inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-#f7fffb border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      className={`inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-[${
+        type === "primary" ? "#f7fffb" : "#000e5c"
+      }] border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
